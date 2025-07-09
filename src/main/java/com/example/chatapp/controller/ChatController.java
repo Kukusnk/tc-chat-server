27cb2c439167d260786caccf08d4aa7adf7664f8
nik <sony.epam@gmail.com>
2025-07-09 13:24:28 +0300

    Added docker-compose, dockerfile, .env files, configured connection to db in application.yml

--- src/main/java/com/example/chatapp/controller/ChatController.java
+++ src/main/java/com/example/chatapp/controller/ChatController.java
@@ -7,7 +7,6 @@ import com.example.chatapp.model.dto.SendMessageDTO;
 import com.example.chatapp.util.DevTools;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
